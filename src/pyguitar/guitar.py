09bf34e0c10ca84559f1780446c4dcc02be278d4
Jeremy Lainé <jeremy.laine@m4x.org>
2021-09-07 15:48:32 +0200

    [guitar] add SVG export of fret board

--- src/pyguitar/guitar.py
+++ src/pyguitar/guitar.py
@@ -5,26 +5,80 @@ from pychord.utils import note_to_val
 
 from pyguitar.notes import Note
 
-FRETS = 15
+FRETS = 18
 STRINGS = [Note.E2, Note.A2, Note.D3, Note.G3, Note.B3, Note.E4]
 
 
 class Fretboard:
     def __init__(self):
-        self._cells = [["" for x in STRINGS] for f in range(FRETS)]
+        self._cells = [[None for x in STRINGS] for f in range(FRETS)]
 
     def dump(self):
         indent = "   "
         width = 31
         lines = []
         for idx, row in enumerate(self._cells):
-            row = ["%2s" % x for x in row]
+            row = ["%2s" % (x["text"] if x else "") for x in row]
             lines.append(("%.2d | " % idx) + (" | ".join(row)) + " |")
             if idx:
                 lines.append(indent + ("-" * width))
             else:
                 lines.append(indent + ("=" * width))
         return "\n".join(lines)
+
+    def dump_svg(self):
+        padding = 10
+        fret_spacing = 30
+        string_spacing = 20
+        board_width = string_spacing * (len(STRINGS) - 1)
+        board_height = fret_spacing * FRETS
+        output = '<svg viewBox="0 0 %d %d" xmlns="http://www.w3.org/2000/svg">' % (
+            board_width + 2 * padding,
+            board_height + 2 * padding,
+        )
+
+        # draw strings
+        for string_idx, string_note in enumerate(STRINGS):
+            x = padding + string_idx * string_spacing
+            output += '<line x1="%d" y1="%d" x2="%d" y2="%d" stroke="black"/>' % (
+                x,
+                padding,
+                x,
+                padding + board_height,
+            )
+
+        # draw frets
+        for fret_idx in range(FRETS + 1):
+            y = padding + fret_idx * fret_spacing
+            output += '<line x1="%d" y1="%d" x2="%d" y2="%d" stroke="black"/>' % (
+                padding,
+                y,
+                padding + board_width,
+                y,
+            )
+
+        # draw markers
+        for fret_idx, row in enumerate(self._cells):
+            for string_idx, cell in enumerate(row):
+                if cell:
+                    cx = padding + string_idx * string_spacing
+                    cy = padding + (fret_idx + 0.5) * fret_spacing
+                    output += (
+                        '<circle cx="%d" cy="%d" r="%d" stroke="%s" fill="white" />'
+                        % (
+                            cx,
+                            cy,
+                            string_spacing / 2.5,
+                            cell["color"],
+                        )
+                    )
+                    output += (
+                        '<text x="%d" y="%d" fill="%s" text-anchor="middle">%s</text>'
+                        % (cx, cy + 4, cell["color"], cell["text"])
+                    )
+
+        output += "</svg>"
+        return output
 
     def set(self, pos, value):
         self._cells[pos[0]][pos[1]] = value
@@ -43,6 +97,7 @@ if __name__ == "__main__":
     board = Fretboard()
     chord = Chord(options.chord)
 
+    note_colors = ["red", "green", "blue", "black"]
     note_names = chord.components()
     note_values = [note_to_val(x) for x in note_names]
 
@@ -52,5 +107,8 @@ if __name__ == "__main__":
             idx = note_values.index(note_value % 12)
         except ValueError:
             continue
-        board.set(pos, note_names[idx])
+        board.set(pos, {"color": note_colors[idx], "text": note_names[idx]})
     print(board.dump())
+
+    with open("board.svg", "w") as fp:
+        fp.write(board.dump_svg())
