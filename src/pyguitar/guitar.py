effd839538a2db2f1b308c32b1cbdd9fc9f8a593
Jeremy Lainé <jeremy.laine@m4x.org>
2025-07-20 11:45:35 +0200

    Rotate fretboard

--- src/pyguitar/guitar.py
+++ src/pyguitar/guitar.py
@@ -1,4 +1,5 @@
 import dataclasses
+import enum
 from typing import Iterator, Optional
 
 from colorama import Back, Fore, Style
@@ -15,13 +16,57 @@ class Cell:
     text: str
 
 
+class Orientation(enum.Enum):
+    PORTRAIT = "PORTRAIT"
+    LANDSCAPE = "LANDSCAPE"
+
+
 class Fretboard:
     def __init__(self) -> None:
         self._cells: list[list[Optional[Cell]]] = [
             [None for x in STRINGS] for f in range(FRETS)
         ]
 
-    def dump(self) -> str:
+    def dump_ansi(self, *, orientation: Orientation) -> str:
+        if orientation == Orientation.LANDSCAPE:
+            return self.dump_ansi_landscape()
+        else:
+            return self.dump_ansi_portrait()
+
+    def dump_ansi_landscape(self) -> str:
+        def pad(i: str) -> str:
+            if len(i) == 1:
+                return "-" + i + "-"
+            elif len(i) == 2:
+                return "-" + i
+            else:
+                return i
+
+        lines = []
+        empty_line = "   ||" + ("   |" * (FRETS - 1))
+        for string_idx in range(len(STRINGS) - 1, -1, -1):
+            cells = [row[string_idx] for row in self._cells]
+            line = "".join(
+                (
+                    getattr(Fore, cell.color.upper()) + pad(cell.text) + Fore.BLACK
+                    if cell is not None
+                    else (Fore.BLACK + pad("-") + Fore.RESET)
+                )
+                + Fore.BLACK
+                + ("|" if idx else "||")
+                for idx, cell in enumerate(cells)
+            )
+            lines.append(Back.WHITE + line)
+            if string_idx:
+                lines.append(Back.WHITE + Fore.BLACK + empty_line)
+            else:
+                lines.append(
+                    Fore.WHITE
+                    + "".join([f"{i:02}  " + ("" if i else " ") for i in range(FRETS)])
+                )
+        return "".join(line + Style.RESET_ALL + "\n" for line in lines)
+
+    def dump_ansi_portrait(self) -> str:
         def pad(i: str) -> str:
             if len(i) == 1:
                 return " " + i + " "
@@ -34,33 +79,40 @@ class Fretboard:
         lines = []
         width = 5 * len(STRINGS) - 2
         for idx, row in enumerate(self._cells):
-            str_row: list[str] = [
+            line = "  ".join(
                 (
                     (getattr(Fore, cell.color.upper()) + pad(cell.text) + Fore.BLACK)
                     if cell is not None
                     else (Fore.BLACK + pad("|") + Fore.RESET)
                 )
                 for cell in row
-            ]
-            lines.append(
-                ("%.2d " % idx) + Back.WHITE + "  ".join(str_row) + Style.RESET_ALL
             )
+            # lines.append(("%.2d " % idx) + Back.WHITE + row_line)
+            lines.append(f"{idx:02} " + Back.WHITE + line)
             marker = "-" if idx else "="
-            lines.append(
-                indent + Back.WHITE + Fore.BLACK + (marker * width) + Style.RESET_ALL
-            )
-        return "\n".join(lines)
+            lines.append(indent + Back.WHITE + Fore.BLACK + (marker * width))
+        return "".join(line + Style.RESET_ALL + "\n" for line in lines)
 
-    def dump_svg(self) -> str:
+    def dump_svg(self, *, orientation: Orientation) -> str:
         padding = 10
         fret_spacing = 30
         string_spacing = 20
         board_width = string_spacing * (len(STRINGS) - 1)
         board_height = fret_spacing * FRETS
-        output = '<svg viewBox="0 0 %f %f" xmlns="http://www.w3.org/2000/svg">' % (
-            board_width + 2 * padding,
-            board_height + 2 * padding,
-        )
+        image_width = board_width + 2 * padding
+        image_height = board_height + 2 * padding
+
+        if orientation == Orientation.LANDSCAPE:
+            svg_transform = f"translate(0, {image_width}) rotate(-90, 0, 0)"
+            svg_viewbox = f"0 0 {image_height} {image_width}"
+            text_angle = 90
+        else:
+            svg_transform = ""
+            svg_viewbox = f"0 0 {image_width} {image_height}"
+            text_angle = 0
+
+        output = f'<svg viewBox="{svg_viewbox}" xmlns="http://www.w3.org/2000/svg">'
+        output += f'<g transform="{svg_transform}">'
 
         # draw strings
         for string_idx, string_note in enumerate(STRINGS):
@@ -98,11 +150,13 @@ class Fretboard:
                         )
                     )
                     output += (
-                        '<text x="%f" y="%f" fill="%s" font-family="arial" '
-                        'font-size="12px" text-anchor="middle">%s</text>'
-                    ) % (cx, cy + 4, cell.color, cell.text)
+                        f'<text x="{cx}" y="{cy + 4}" fill="{cell.color}"'
+                        ' font-family="arial" font-size="12px" text-anchor="middle"'
+                        f' transform="rotate({text_angle}, {cx}, {cy})">'
+                        f"{cell.text}</text>"
+                    )
 
-        output += "</svg>"
+        output += "</g></svg>"
         return output
 
     def set(self, pos: tuple[int, int], value: Optional[Cell]) -> None:
